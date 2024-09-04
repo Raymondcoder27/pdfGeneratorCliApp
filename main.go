cf2bd0dde761cfc54a8f7021348d5994da55a600
MwebeCoder27 <mwebraymond13@gmail.com>
2024-09-04 13:01:27 +0300

    init

--- main.go
+++ main.go
@@ -28,6 +28,13 @@ func createBill() bill {
 	return b
 }
 
+func promptOptions(b bill) {
+	reader := bufio.NewReader(os.Stdin)
+
+	opt, _ := getInput("Choose option (a - add item, s - save bill, t - add tip): ", reader)
+	fmt.Println(opt)
+}
+
 func main() {
 	myBill := createBill()
 
