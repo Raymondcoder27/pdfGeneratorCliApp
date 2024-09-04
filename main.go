97d5f49da95b3deb7282450302e3b227b84349b7
MwebeCoder27 <mwebraymond13@gmail.com>
2024-09-04 12:54:42 +0300

    init

--- main.go
+++ main.go
@@ -1,13 +1,35 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
+
+func getInput(prompt string, r *bufio.Reader) (string, error) {
+	fmt.Print(prompt)
+	input, err := r.ReadString('\n')
+
+	return strings.TrimSpace(input), err
+}
+
+func createBill() bill {
+	reader := bufio.NewReader(os.Stdin)
+
+	// fmt.Print("Create a new bill:")
+	// name, _ := reader.ReadString('\n')
+	// name = strings.TrimSpace(name)
+	name, _ := getInput("Create a new bill name: ", reader)
+
+	b := newBill(name)
+	fmt.Println("Created the bill", b.name)
+
+	return b
+}
 
 func main() {
-	myBill := newBill("My Bill")
-	myBill.updateTip(40)
-	myBill.addItem("onions", 23)
-	myBill.addItem("carrot", 49)
-	myBill.addItem("pineapple", 14)
+	myBill := createBill()
 
 	fmt.Println(myBill.format())
 }
